59e286c05206dec3d25f01cb93023cfa0b018668
LLLBlue <971772794@qq.com>
2018-02-05 16:02:53 +0800

    revert

--- src/main/java/com/blue/yw/repository/NominationListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blue.yw.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NominationListRepository extends JpaRepository<NominationListRepository, Integer> {
-}
